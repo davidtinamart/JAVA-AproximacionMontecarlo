9deb2d925b8c18e920089beafe673ce6ed88ac57
davidtinamart <dmarttin@myuax.com>
2023-03-23 19:50:35 +0100

    Fin Clase Matematicas paquete mates

--- src/main/java/mates/Matematicas.java
+++ src/main/java/mates/Matematicas.java
@@ -3,4 +3,24 @@ import java.util.Random;
 
 public class Matematicas {
 
+    public double generarNumeroPi(long pasos) {
+        long aciertos = 0;
+        double x, y;
+        double areaCuadrado = 4;
+
+        Random aleatorio = new Random();
+
+        for (long i = 0; i < pasos; i++) {
+            x = aleatorio.nextDouble() * 2 - 1;
+            y = aleatorio.nextDouble() * 2 - 1;
+            if (Math.pow(x, 2) + Math.pow(y, 2) <= 1) {
+                aciertos++;
+            }
+        }
+
+        double aproximacionPi = areaCuadrado * (double)aciertos / (double)pasos;
+
+        return aproximacionPi;
+    }
+
 }
