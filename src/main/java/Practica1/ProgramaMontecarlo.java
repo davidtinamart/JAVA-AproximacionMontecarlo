c0652b3cc805dd4a17bc9cbd932d5369b910f179
davidtinamart <dmarttin@myuax.com>
2023-03-23 19:23:13 +0100

    Metodo AproximacionMontecarlo

--- src/main/java/Practica1/ProgramaMontecarlo.java
+++ src/main/java/Practica1/ProgramaMontecarlo.java
@@ -4,6 +4,11 @@ public class ProgramaMontecarlo {
 
     public static void main(String[] args) {
 
+    AproximacionMontecarlo objeto1 = new AproximacionMontecarlo();
+
+    int PuntosTotales=1000000;
+    System.out.println(objeto1.aproximarPorMontecarlo(PuntosTotales));
+
 
     }
 }
